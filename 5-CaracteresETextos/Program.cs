9dadf6dab59c838316f3bcb157806de7481ed06c
J3eff <jeffersonvideo125@gmail.com>
2022-03-01 23:49:35 -0300

    Caracteres e textos

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 -  caracteres e textos");
+
+            // character
+            char primeiraLetra = ' '; // Devemos sempre usar aspas simples
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);
+
+            //primeiraLetra += (char)1; mais imples
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de tecnologia " + 2022;           
+            Console.WriteLine(titulo);
+
+            string cursosProgramacao = @" - .NET
+ - Java
+ - JavaScript";
+            Console.WriteLine(cursosProgramacao);
+            
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
